456ac6003fefc6cf2b3343e1e34b7ec0b1eefcd7
alexandre-toshiro <toshiro.lyn@gmail.com>
2025-04-03 22:05:19 -0300

    Implementando as classes do DDD

---
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/Entity.java
@@ -0,0 +1,30 @@
+package com.fullcycle.admin.catalogo.domain;
+
+import java.util.Objects;
+
+// qualquer classe que extenda de Identifier
+public abstract class Entity<ID extends Identifier> {
+
+    protected final ID id;
+
+    protected Entity(final ID id) {
+        Objects.requireNonNull(id,"'id' cannot be null");
+        this.id = id;
+    }
+
+    public ID getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        final Entity<?> entity = (Entity<?>) o;
+        return Objects.equals(getId(), entity.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+}
---
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/ValueObject.java
@@ -0,0 +1,8 @@
+package com.fullcycle.admin.catalogo.domain;
+
+public abstract class ValueObject {
+    /*
+    A classe é abstrata e não uma interface, pois por uma interface seria possível que um objeto implementasse
+    ValueObject e uma Entity. E isso não é permitido, ou é um ou o outro.
+     */
+}
