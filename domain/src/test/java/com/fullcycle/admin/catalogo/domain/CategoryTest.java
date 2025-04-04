73db656797181d90bb3b4748ef1007ef6ee7fad2
alexandre-toshiro <toshiro.lyn@gmail.com>
2025-04-03 21:31:52 -0300

    Criando a entidade de categoria com TDD

--- domain/src/test/java/com/fullcycle/admin/catalogo/domain/CategoryTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/CategoryTest.java
@@ -1,12 +1,27 @@
 package com.fullcycle.admin.catalogo.domain;
 
+import com.fullcycle.admin.catalogo.domain.category.Category;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CategoryTest {
 
     @Test
-    public void test() {
-        Assertions.assertNotNull(new Category());
+    public void givenAValidParams_whenCallNewCaregory_thenInstantiateACategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+
     }
 }
