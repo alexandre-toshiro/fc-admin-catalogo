c880fb298d5617c11aca042672e33bf8199e6204
alexandre-toshiro <toshiro.lyn@gmail.com>
2025-04-06 16:59:58 -0300

    Implementando notification patterna como um validation handler

--- application/src/test/java/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
@@ -1,6 +1,5 @@
 package com.fullcycle.admin.catalogo.application.category.create;
 
-import com.fullcycle.admin.catalogo.domain.category.Category;
 import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
 import com.fullcycle.admin.catalogo.domain.exceptions.DomainException;
 import org.junit.jupiter.api.Assertions;
