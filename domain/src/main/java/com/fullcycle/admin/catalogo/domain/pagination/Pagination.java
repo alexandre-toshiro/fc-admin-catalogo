058c4c83cfb9a5bb6adff3d6c39977fafc81fbc1
alexandre-toshiro <toshiro.lyn@gmail.com>
2025-04-07 21:15:26 -0300

    Implementando o caso de uso de listagem de categorias

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/pagination/Pagination.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/pagination/Pagination.java
@@ -1,6 +1,7 @@
 package com.fullcycle.admin.catalogo.domain.pagination;
 
 import java.util.List;
+import java.util.function.Function;
 
 public record Pagination<T>(
         int currentPage, //Pagina atual
@@ -8,4 +9,12 @@ public record Pagination<T>(
         long total, // quantas infos no BD
         List<T> items // os itens em si no retorno do BD
 ) {
+    public <R> Pagination<R> map(final Function<T, R> mapper) {
+        List<R> aNewList = this.items.stream()
+                .map(mapper)
+                .toList();
+
+        return new Pagination<>(currentPage(), perPage(), total(), aNewList);
+
+    }
 }
