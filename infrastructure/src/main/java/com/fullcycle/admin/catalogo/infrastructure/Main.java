0d697243abd4177c0afdb6fc971757cc7cba8f19
alexandre-toshiro <toshiro.lyn@gmail.com>
2025-04-19 20:21:41 -0300

    Injetando a implementacao do gateway nos casos de uso

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -1,8 +1,17 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
+import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryUseCase;
+import com.fullcycle.admin.catalogo.application.category.delete.DeleteCategoryUseCase;
+import com.fullcycle.admin.catalogo.application.category.retrieve.get.GetCategoryByIdUseCase;
+import com.fullcycle.admin.catalogo.application.category.retrieve.list.ListCategoriesUseCase;
+import com.fullcycle.admin.catalogo.application.category.update.UpdateCategoryUseCase;
 import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
@@ -11,5 +20,18 @@ public class Main {
         //System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "development");
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
+    }
+
+    //tese para verificar se a injeção dos beans esta correta
+    @Bean
+    @DependsOnDatabaseInitialization
+    ApplicationRunner runner(@Autowired CreateCategoryUseCase createCategoryUseCase,
+                             @Autowired UpdateCategoryUseCase updateCategoryUseCase,
+                             @Autowired DeleteCategoryUseCase deleteCategoryUseCase,
+                             @Autowired ListCategoriesUseCase ListCategoriesUseCase,
+                             @Autowired GetCategoryByIdUseCase getCategoryByIdUseCase) {
+        return args -> {
+
+        };
     }
 }
